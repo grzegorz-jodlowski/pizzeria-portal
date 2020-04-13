fab579e96c48147c0ab939c306ce6e9fba56b269
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-04-13 18:21:42 +0200

    Implement settings of Maretial UI

--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import PageNav from '../PageNav/PageNav';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 
-const MainLayout = props => {
+
+const MainLayout = ({ children }) => {
   return (
-    <div>
-      <PageNav />
-      <div>{props.children}</div>
+    <div className='MainLayout'>
+      <AppBar>
+        <Toolbar>
+          <PageNav />
+        </Toolbar>
+      </AppBar>
+      <div>{children}</div>
     </div>
   );
 };
