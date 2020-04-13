77577869a01cf3705112bcb02baf69610eae527a
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-04-13 19:16:20 +0200

    Adapt site to Material UI

--- src/App.js
+++ src/App.js
@@ -14,27 +14,42 @@ import NewEvent from './components/views/NewEvent/NewEvent';
 import Booking from './components/views/Booking/Booking';
 import Events from './components/views/Events/Events';
 import { StylesProvider } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#2B4C6F',
+    },
+    // secondary: {
+    //   main: '#11cb5f',
+    // },
+  },
+});
 
 function App() {
   return (
     <BrowserRouter>
       <StylesProvider injectFirst>
-        <MainLayout>
-          <Switch>
-            <Route exact path={`${process.env.PUBLIC_URL}/`} component={Homepage} />
-            <Route exact path={`${process.env.PUBLIC_URL}/login`} component={Login} />
-            <Route exact path={`${process.env.PUBLIC_URL}/tables`} component={Tables} />
-            <Route exact path={`${process.env.PUBLIC_URL}/tables/booking/new`} component={NewBooking} />
-            <Route exact path={`${process.env.PUBLIC_URL}/tables/booking/:id`} component={Booking} />
-            <Route exact path={`${process.env.PUBLIC_URL}/tables/events/new`} component={NewEvent} />
-            <Route exact path={`${process.env.PUBLIC_URL}/tables/events/:id`} component={Events} />
-            <Route exact path={`${process.env.PUBLIC_URL}/kitchen`} component={Kitchen} />
-            <Route exact path={`${process.env.PUBLIC_URL}/waiter`} component={Waiter} />
-            <Route exact path={`${process.env.PUBLIC_URL}/ordering`} component={Ordering} />
-            <Route exact path={`${process.env.PUBLIC_URL}/ordering/new`} component={NewOrder} />
-            <Route exact path={`${process.env.PUBLIC_URL}/ordering/order/:id`} component={Order} />
-          </Switch>
-        </MainLayout>
+        <ThemeProvider theme={theme}>
+          <MainLayout>
+            <Switch>
+              <Route exact path={`${process.env.PUBLIC_URL}/`} component={Homepage} />
+              <Route exact path={`${process.env.PUBLIC_URL}/login`} component={Login} />
+              <Route exact path={`${process.env.PUBLIC_URL}/tables`} component={Tables} />
+              <Route exact path={`${process.env.PUBLIC_URL}/tables/booking/new`} component={NewBooking} />
+              <Route exact path={`${process.env.PUBLIC_URL}/tables/booking/:id`} component={Booking} />
+              <Route exact path={`${process.env.PUBLIC_URL}/tables/events/new`} component={NewEvent} />
+              <Route exact path={`${process.env.PUBLIC_URL}/tables/events/:id`} component={Events} />
+              <Route exact path={`${process.env.PUBLIC_URL}/kitchen`} component={Kitchen} />
+              <Route exact path={`${process.env.PUBLIC_URL}/waiter`} component={Waiter} />
+              <Route exact path={`${process.env.PUBLIC_URL}/ordering`} component={Ordering} />
+              <Route exact path={`${process.env.PUBLIC_URL}/ordering/new`} component={NewOrder} />
+              <Route exact path={`${process.env.PUBLIC_URL}/ordering/order/:id`} component={Order} />
+            </Switch>
+          </MainLayout>
+        </ThemeProvider>
       </StylesProvider>
     </BrowserRouter>
   );
--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -3,18 +3,23 @@ import PropTypes from 'prop-types';
 import PageNav from '../PageNav/PageNav';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Container from '@material-ui/core/Container';
 
 
 const MainLayout = ({ children }) => {
   return (
     <div className='MainLayout'>
       <AppBar>
-        <Toolbar>
-          <PageNav />
-        </Toolbar>
+        <Container maxWidth='lg'>
+          <Toolbar disableGutters>
+            <PageNav />
+          </Toolbar>
+        </Container>
       </AppBar>
-      <Toolbar />
-      <div>{children}</div>
+      <Container maxWidth='lg'>
+        <Toolbar />
+        {children}
+      </Container>
     </div>
   );
 };
