56ede5d1558ee751720b3bb7fb13618782c1803f
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-04-13 18:31:51 +0200

    Refactor PageNav to Material UI

--- src/components/layout/PageNav/PageNav.js
+++ src/components/layout/PageNav/PageNav.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import styles from './PageNav.module.scss';
 import { NavLink } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+
 
 const PageNav = () => {
   return (
-    <nav>
-      <NavLink exact to={`/`} activeClassName='active'>Home</NavLink>
-      <NavLink to={`/login`} activeClassName='active'>Login</NavLink>
-      <NavLink to={`/tables`} activeClassName='active'>Tables</NavLink>
-      <NavLink to={`/waiter`} activeClassName='active'>Waiter</NavLink>
-      <NavLink to={`/kitchen`} activeClassName='active'>Kitchen</NavLink>
-      <NavLink to={`/ordering`} activeClassName='active'>Ordering</NavLink>
+    <nav className={styles.component}>
+      <Button className={styles.link} component={NavLink} exact to={`/`} activeClassName='active'>Dashboard</Button>
+      <Button className={styles.link} component={NavLink} to={`/login`} activeClassName='active'>Login</Button>
+      <Button className={styles.link} component={NavLink} to={`/tables`} activeClassName='active'>Tables</Button>
+      <Button className={styles.link} component={NavLink} to={`/waiter`} activeClassName='active'>Waiter</Button>
+      <Button className={styles.link} component={NavLink} to={`/kitchen`} activeClassName='active'>Kitchen</Button>
     </nav>
   );
 };
