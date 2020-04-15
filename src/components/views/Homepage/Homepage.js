07efc35971afabc248407b784684042d8c182dd8
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-04-15 17:42:11 +0200

    Add boxes with orders in Dashboard view

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -1,11 +1,53 @@
 import React from 'react';
 import styles from './Homepage.module.scss';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+
+const localOrders = [
+  { id: '1', status: 'free', order: null },
+  { id: '2', status: 'thinking', order: null },
+  { id: '3', status: 'ordered', order: 123 },
+  { id: '4', status: 'prepared', order: 234 },
+  { id: '5', status: 'delivered', order: 345 },
+  { id: '6', status: 'paid', order: 456 },
+];
+
+const remoteOrders = [
+  { id: '1', status: 'free', order: null },
+  { id: '2', status: 'thinking', order: null },
+  { id: '3', status: 'ordered', order: 123 },
+];
+
+const date = new Date();
 
 const Homepage = () => {
   return (
-    <div className={styles.component}>
-      <h2>Homepage view</h2>
-    </div>
+    <main className={styles.component}>
+      <section className={styles.ordersBoxes}>
+        <Paper className={styles.ordersBox}>
+          <Typography component="h2" variant="h6" color="primary">
+            Local orders
+          </Typography>
+          <Typography component="p" variant="h4">
+            {localOrders.length}
+          </Typography>
+          <Typography color="textSecondary">
+            {date.toLocaleDateString()}
+          </Typography>
+        </Paper>
+        <Paper className={styles.ordersBox}>
+          <Typography component="h2" variant="h6" color="primary">
+            Remote orders
+          </Typography>
+          <Typography component="p" variant="h4">
+            {remoteOrders.length}
+          </Typography>
+          <Typography color="textSecondary">
+            {date.toLocaleDateString()}
+          </Typography>
+        </Paper>
+      </section>
+    </main>
   );
 };
 
