3c9e1af943aa46aec8ee13c6524de9a9a77dd6ba
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-04-15 18:01:04 +0200

    Adapt demoContent to Dashboard subpage

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -2,20 +2,38 @@ import React from 'react';
 import styles from './Homepage.module.scss';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
 
 const localOrders = [
-  { id: '1', status: 'free', order: null },
-  { id: '2', status: 'thinking', order: null },
-  { id: '3', status: 'ordered', order: 123 },
-  { id: '4', status: 'prepared', order: 234 },
-  { id: '5', status: 'delivered', order: 345 },
-  { id: '6', status: 'paid', order: 456 },
+  { id: '1' },
+  { id: '2' },
+  { id: '3' },
+  { id: '4' },
+  { id: '5' },
+  { id: '6' },
 ];
 
 const remoteOrders = [
-  { id: '1', status: 'free', order: null },
-  { id: '2', status: 'thinking', order: null },
-  { id: '3', status: 'ordered', order: 123 },
+  { id: '1' },
+  { id: '2' },
+  { id: '3' },
+  { id: '4' },
+];
+
+const demoContent = [
+  { id: '1', type: 'Event', time: '9-11', people: 4, name: 'John Smith', table: '1' },
+  { id: '2', type: 'Booking', time: '11', people: 4, name: 'John Smith', table: '2' },
+  { id: '3', type: 'Event', time: '9-11', people: 4, name: 'John Smith', table: '3' },
+  { id: '4', type: 'Booking', time: '9-11', people: 4, name: 'John Smith', table: '4' },
+  { id: '5', type: 'Booking', time: '9-11', people: 4, name: 'John Smith', table: '1' },
+  { id: '6', type: 'Event', time: '9-11', people: 4, name: 'John Smith', table: '2' },
+  { id: '7', type: 'Event', time: '9-11', people: 4, name: 'John Smith', table: '3' },
 ];
 
 const date = new Date();
@@ -47,6 +65,45 @@ const Homepage = () => {
           </Typography>
         </Paper>
       </section>
+      <Paper className={styles.component}>
+        <Typography component="h2" variant="h6" color="primary">
+          Today&apos;s reservations and events
+        </Typography>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Id</TableCell>
+              <TableCell>Type</TableCell>
+              <TableCell>Time</TableCell>
+              <TableCell>People</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>Table</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {demoContent.map(row => (
+              <TableRow key={row.id}>
+                <TableCell component="th" scope="row">
+                  {row.id}
+                </TableCell>
+                <TableCell>
+                  {row.status}
+                </TableCell>
+                <TableCell>
+                  {row.order && (
+                    <Button component={Link} to={`${process.env.PUBLIC_URL}/waiter/order/${row.order}`}>
+                      {row.order}
+                    </Button>
+                  )}
+                </TableCell>
+                <TableCell>
+                  {/* {renderActions(row.status)} */}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Paper>
     </main>
   );
 };
