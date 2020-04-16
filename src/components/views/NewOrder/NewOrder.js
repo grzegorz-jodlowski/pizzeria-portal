051480699cba8b4d34431be1cd59b79623fdbf37
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-04-16 12:23:49 +0200

    Add demo content and Table input to NewOrder comp

--- src/components/views/NewOrder/NewOrder.js
+++ src/components/views/NewOrder/NewOrder.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import styles from './NewOrder.module.scss';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
+const demoTables = [
+  { id: '1' },
+  { id: '2' },
+  { id: '3' },
+  { id: '4' },
+  { id: '5' },
+  { id: '6' },
+];
 
 const NewOrder = () => {
   return (
     <div className={styles.component}>
-      <h2>NewOrder view</h2>
+      <FormControl required className={styles.formControl}>
+        <InputLabel id="table-select-label">Table</InputLabel>
+        <Select
+          labelId="table-select-label"
+          id="table-select"
+        // value={age}
+        // onChange={handleChange}
+        >
+          {demoTables.map(table => (
+            <MenuItem key={table.id} value={table.id}>{table.id}</MenuItem>
+          ))}
+        </Select>
+      </FormControl>
     </div>
   );
 };
