39bb8c078a4b57d8441337827dd919feeae03b36
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-04-17 18:16:48 +0200

    Add date picker to Tables component

--- src/components/views/Tables/Tables.js
+++ src/components/views/Tables/Tables.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import styles from './Tables.module.scss';
 import { Link } from 'react-router-dom';
+import TextField from '@material-ui/core/TextField';
+
+const today = new Date();
 
 const Tables = () => {
   const state = {
     id: 'abc',
+    date: `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`,
   };
 
   return (
     <div className={styles.component}>
+      <form className={styles.formContainer} noValidate>
+        <TextField
+          id="date"
+          label="Date"
+          type="date"
+          defaultValue={state.date}
+          className={styles.textField}
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+      </form>
       <h2>Tables view</h2>
       <Link to={`${process.env.PUBLIC_URL}/tables/booking/new`} className={styles.link}>
         New booking
