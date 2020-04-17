e3cc03ee6b81bbd1c1d2a1101694b91eb1d5c052
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-04-17 17:14:55 +0200

    Refactor NewBooking. NewEvent to Tables

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ import Tables from './components/views/Tables/Tables';
 import Waiter from './components/views/Waiter/Waiter';
 import Homepage from './components/views/Homepage/Homepage';
 import WaiterOrder from './components/views/WaiterOrder/WaiterOrder';
-import NewBooking from './components/views/NewBooking/NewBooking';
-import NewEvent from './components/views/NewEvent/NewEvent';
 import TablesBooking from './components/views/TablesBooking/TablesBooking';
 import TablesEvent from './components/views/TablesEvent/TablesEvent';
 import { StylesProvider } from '@material-ui/core/styles';
@@ -36,9 +34,7 @@ function App() {
               <Route exact path={`${process.env.PUBLIC_URL}/`} component={Homepage} />
               <Route exact path={`${process.env.PUBLIC_URL}/login`} component={Login} />
               <Route exact path={`${process.env.PUBLIC_URL}/tables`} component={Tables} />
-              <Route exact path={`${process.env.PUBLIC_URL}/tables/booking/new`} component={NewBooking} />
               <Route exact path={`${process.env.PUBLIC_URL}/tables/booking/:id`} component={TablesBooking} />
-              <Route exact path={`${process.env.PUBLIC_URL}/tables/events/new`} component={NewEvent} />
               <Route exact path={`${process.env.PUBLIC_URL}/tables/events/:id`} component={TablesEvent} />
               <Route exact path={`${process.env.PUBLIC_URL}/kitchen`} component={Kitchen} />
               <Route exact path={`${process.env.PUBLIC_URL}/waiter`} component={Waiter} />
