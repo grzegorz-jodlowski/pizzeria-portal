2bfadfde4004d89604702e1e13191ea3e40a8a60
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-04-18 09:52:55 +0200

    Refactor Tables to use only TablesBooking comp

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import Waiter from './components/views/Waiter/Waiter';
 import Homepage from './components/views/Homepage/Homepage';
 import WaiterOrder from './components/views/WaiterOrder/WaiterOrder';
 import TablesBooking from './components/views/TablesBooking/TablesBooking';
-import TablesEvent from './components/views/TablesEvent/TablesEvent';
 import { StylesProvider } from '@material-ui/core/styles';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
@@ -35,7 +34,7 @@ function App() {
               <Route exact path={`${process.env.PUBLIC_URL}/login`} component={Login} />
               <Route exact path={`${process.env.PUBLIC_URL}/tables`} component={Tables} />
               <Route exact path={`${process.env.PUBLIC_URL}/tables/booking/:id`} component={TablesBooking} />
-              <Route exact path={`${process.env.PUBLIC_URL}/tables/event/:id`} component={TablesEvent} />
+              <Route exact path={`${process.env.PUBLIC_URL}/tables/event/:id`} component={TablesBooking} />
               <Route exact path={`${process.env.PUBLIC_URL}/kitchen`} component={Kitchen} />
               <Route exact path={`${process.env.PUBLIC_URL}/waiter`} component={Waiter} />
               <Route exact path={`${process.env.PUBLIC_URL}/waiter/order/:id`} component={WaiterOrder} />
