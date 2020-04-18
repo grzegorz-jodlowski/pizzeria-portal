4c35e142e6a4bb7ee04c297cfb6796f618f24975
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-04-18 08:50:37 +0200

    Add demoContent to TablesBooking

--- src/components/views/TablesBooking/TablesBooking.js
+++ src/components/views/TablesBooking/TablesBooking.js
@@ -2,6 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './TablesBooking.module.scss';
 
+const demoContent = [
+  { id: '1', type: 'event', duration: 2, startTime: 9, people: 4, name: 'John Smith', table: '1', status: 'waiting' },
+  { id: '2', type: 'booking', duration: 1, startTime: 11, people: 4, name: 'John Smith', table: '2', status: 'waiting' },
+  { id: '3', type: 'event', duration: null, startTime: 11, people: 4, name: 'John Smith', table: '3', status: 'waiting' },
+  { id: '4', type: 'booking', duration: 1.5, startTime: 14, people: 4, name: 'John Smith', table: '4', status: 'waiting' },
+  { id: '5', type: 'booking', duration: 2, startTime: 15, people: 4, name: 'John Smith', table: '1', status: 'waiting' },
+  { id: '6', type: 'event', duration: 2, startTime: 17, people: 4, name: 'John Smith', table: '2', status: 'waiting' },
+  { id: '7', type: 'event', duration: 2, startTime: 19, people: 4, name: 'John Smith', table: '3', status: 'waiting' },
+];
+
 const TablesBooking = ({ match }) => {
   return (
     <div className={styles.component}>
