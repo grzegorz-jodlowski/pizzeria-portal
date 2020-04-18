cfbcd4128f938b5c9b518380ad4411ad8eb1e061
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-04-18 11:18:36 +0200

    Add conditional rendering in Waiter with API data

--- src/components/views/Waiter/Waiter.js
+++ src/components/views/Waiter/Waiter.js
@@ -10,95 +10,109 @@ import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
 
-const demoContent = [
-  { id: '1', status: 'free', order: null },
-  { id: '2', status: 'thinking', order: null },
-  { id: '3', status: 'ordered', order: 123 },
-  { id: '4', status: 'prepared', order: 234 },
-  { id: '5', status: 'delivered', order: 345 },
-  { id: '6', status: 'paid', order: 456 },
-];
-
-const renderActions = status => {
-  switch (status) {
-    case 'free':
-      return (
-        <>
-          <Button>thinking</Button>
-          <Button component={Link} to={`${process.env.PUBLIC_URL}/waiter/order/new`}>new order</Button>
-        </>
-      );
-    case 'thinking':
-      return (
-        <Button component={Link} to={`${process.env.PUBLIC_URL}/waiter/order/new`}>new order</Button>
-      );
-    case 'ordered':
-      return (
-        <Button>prepared</Button>
-      );
-    case 'prepared':
-      return (
-        <Button>delivered</Button>
-      );
-    case 'delivered':
-      return (
-        <Button>paid</Button>
-      );
-    case 'paid':
-      return (
-        <Button>free</Button>
-      );
-    default:
-      return null;
-  }
-};
-
 class Waiter extends React.Component {
   static propTypes = {
+    tables: PropTypes.array,
     fetchTables: PropTypes.func,
     loading: PropTypes.shape({
       active: PropTypes.bool,
-      error: PropTypes.anyOf(PropTypes.bool, PropTypes.string),
+      error: PropTypes.bool || PropTypes.string,
     }),
   }
 
+  componentDidMount() {
+    const { fetchTables } = this.props;
+    fetchTables();
+  }
+
+  renderActions(status) {
+    switch (status) {
+      case 'free':
+        return (
+          <>
+            <Button>thinking</Button>
+            <Button component={Link} to={`${process.env.PUBLIC_URL}/waiter/order/new`}>new order</Button>
+          </>
+        );
+      case 'thinking':
+        return (
+          <Button component={Link} to={`${process.env.PUBLIC_URL}/waiter/order/new`}>new order</Button>
+        );
+      case 'ordered':
+        return (
+          <Button>prepared</Button>
+        );
+      case 'prepared':
+        return (
+          <Button>delivered</Button>
+        );
+      case 'delivered':
+        return (
+          <Button>paid</Button>
+        );
+      case 'paid':
+        return (
+          <Button>free</Button>
+        );
+      default:
+        return null;
+    }
+  }
+
   render() {
-    return (
-      <Paper className={styles.component}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Table</TableCell>
-              <TableCell>Status</TableCell>
-              <TableCell>Order</TableCell>
-              <TableCell>Action</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {demoContent.map(row => (
-              <TableRow key={row.id}>
-                <TableCell component="th" scope="row">
-                  {row.id}
-                </TableCell>
-                <TableCell>
-                  {row.status}
-                </TableCell>
-                <TableCell>
-                  {row.order && (
-                    <Button component={Link} to={`${process.env.PUBLIC_URL}/waiter/order/${row.order}`}>
-                      {row.order}
-                    </Button>
-                  )}
-                </TableCell>
-                <TableCell>
-                  {renderActions(row.status)}
-                </TableCell>
+    const { loading: { active, error }, tables } = this.props;
+
+    if (active || !tables.length) {
+      return (
+        <Paper className={styles.component}>
+          <p>Loading...</p>
+        </Paper>
+      );
+    } else if (error) {
+      return (
+        <Paper className={styles.component}>
+          <p>Error! Details:</p>
+          <pre>{error}</pre>
+        </Paper>
+      );
+    } else {
+      return (
+        <Paper className={styles.component}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Table</TableCell>
+                <TableCell>Status</TableCell>
+                <TableCell>Order</TableCell>
+                <TableCell>Action</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </Paper>
-    );
+            </TableHead>
+            <TableBody>
+              {tables.map(row => (
+                <TableRow key={row.id}>
+                  <TableCell component="th" scope="row">
+                    {row.id}
+                  </TableCell>
+                  <TableCell>
+                    {row.status}
+                  </TableCell>
+                  <TableCell>
+                    {row.order && (
+                      <Button component={Link} to={`${process.env.PUBLIC_URL}/waiter/order/${row.order}`}>
+                        {row.order}
+                      </Button>
+                    )}
+                  </TableCell>
+                  <TableCell>
+                    {this.renderActions(row.status)}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Paper>
+      );
+    }
   }
 }
 
